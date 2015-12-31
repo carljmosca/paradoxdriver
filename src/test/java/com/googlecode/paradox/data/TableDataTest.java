97d4ae640909d5565bf4efe3e51392be86e2232b
Carl J Mosca <carljmosca@gmail.com>
2015-12-31 10:39:43 -0500

    modified to output data

--- src/test/java/com/googlecode/paradox/data/TableDataTest.java
+++ src/test/java/com/googlecode/paradox/data/TableDataTest.java
@@ -18,67 +18,85 @@ import com.googlecode.paradox.test.MainTest;
 
 public class TableDataTest {
 
-	private ParadoxConnection conn;
+    private ParadoxConnection conn;
 
-	@BeforeClass
-	public static void setUp() throws Exception {
-		Class.forName(Driver.class.getName());
-	}
+    @BeforeClass
+    public static void setUp() throws Exception {
+        Class.forName(Driver.class.getName());
+    }
 
-	@Before
-	public void connect() throws Exception {
-		conn = (ParadoxConnection) DriverManager.getConnection(MainTest.CONNECTION_STRING + "db");
-	}
+    @Before
+    public void connect() throws Exception {
+        conn = (ParadoxConnection) DriverManager.getConnection(MainTest.CONNECTION_STRING + "db");
+    }
 
-	@After
-	public void closeConnection() throws Exception {
-		if (conn != null) {
-			conn.close();
-		}
-	}
+    @After
+    public void closeConnection() throws Exception {
+        if (conn != null) {
+            conn.close();
+        }
+    }
 
-	@Test
-	public void testLoadAreaCodes() throws Exception {
-		final ParadoxTable table = TableData.listTables(conn, "areacodes.db").get(0);
-		final ArrayList<ArrayList<FieldValue>> data = TableData.loadData(conn, table, table.getFields());
-		Assert.assertEquals(table.getRowCount(), data.size());
-	}
+    @Test
+    public void testLoadAreaCodes() throws Exception {
+        final ParadoxTable table = TableData.listTables(conn, "areacodes.db").get(0);
+        final ArrayList<ArrayList<FieldValue>> data = TableData.loadData(conn, table, table.getFields());
+        Assert.assertEquals(table.getRowCount(), data.size());
+    }
 
-	@Test
-	public void testLoadContacts() throws Exception {
-		final ParadoxTable table = TableData.listTables(conn, "contacts.db").get(0);
-		final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
-		fields.add(table.getFields().get(0));
-		TableData.loadData(conn, table, fields);
-	}
+    @Test
+    public void testLoadContacts() throws Exception {
+        final ParadoxTable table = TableData.listTables(conn, "contacts.db").get(0);
+        final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
+        fields.add(table.getFields().get(0));
+        TableData.loadData(conn, table, fields);
+    }
 
-	@Test
-	public void testLoadCustomer() throws Exception {
-		final ParadoxTable table = TableData.listTables(conn, "customer.db").get(0);
-		final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
-		fields.add(table.getFields().get(0));
-		TableData.loadData(conn, table, fields);
-	}
+    @Test
+    public void testLoadCustomer() throws Exception {
+        final ParadoxTable table = TableData.listTables(conn, "customer.db").get(0);
+        final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
+        fields.add(table.getFields().get(0));
+        TableData.loadData(conn, table, fields);
+    }
 
-	@Test
-	public void testLoadHercules() throws Exception {
-		final ParadoxTable table = TableData.listTables(conn, "hercules.db").get(0);
-		TableData.loadData(conn, table, table.getFields());
-	}
+    @Test
+    public void testLoadHercules() throws Exception {
+        final ParadoxTable table = TableData.listTables(conn, "hercules.db").get(0);
+        TableData.loadData(conn, table, table.getFields());
+    }
 
-	@Test
-	public void testLoadOrders() throws Exception {
-		final ParadoxTable table = TableData.listTables(conn, "orders.db").get(0);
-		final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
-		fields.add(table.getFields().get(0));
-		TableData.loadData(conn, table, fields);
-	}
+    @Test
+    public void testLoadOrders() throws Exception {
+        loadTable("orders.db");
+    }
 
-	@Test
-	public void testLoadServer() throws Exception {
-		final ParadoxTable table = TableData.listTables(conn, "server.db").get(0);
-		final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
-		fields.add(table.getFields().get(0));
-		TableData.loadData(conn, table, fields);
-	}
+    @Test
+    public void testLoadServer() throws Exception {
+        final ParadoxTable table = TableData.listTables(conn, "server.db").get(0);
+        final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
+        fields.add(table.getFields().get(0));
+        TableData.loadData(conn, table, fields);
+    }
+    
+    private void loadTable(String tableName) throws Exception {
+        final ParadoxTable table = TableData.listTables(conn, tableName).get(0);
+        final ArrayList<ParadoxField> fields = new ArrayList<ParadoxField>();
+        fields.addAll(table.getFields());
+        System.out.println(tableName);
+        System.out.print("Fields: ");
+        for (ParadoxField field : fields) {
+            System.out.print(field.getName() + " ");
+        }
+        System.out.println();
+        fields.add(table.getFields().get(0));
+        ArrayList<ArrayList<FieldValue>> fieldValues =TableData.loadData(conn, table, fields);
+        for (int i = 0; i < fieldValues.size(); i++) {
+            for (FieldValue fieldValue : fieldValues.get(i)) {
+                System.out.print(fieldValue.getValue() + " ");
+                
+            }
+            System.out.println();
+        }
+    }
 }
